abf347a4bb5f129db2f667fc4fa498d10b6d2a72
notaravinth <aravinthravichandran2004@gmail.com>
2026-03-07 11:22:51 +0530

    socket-implementation

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-import { WebSocketServer } from "ws"; 
+import { WebSocketServer, WebSocket } from "ws"; 
 
 const wss = new WebSocketServer({port : 8080});
 
 // Connection event 
 wss.on('connection', (socket, request)=>{
-    
-});
+    const ip = request.socket.remoteAddress;
+    socket.on('message', (rawData) =>{
+        const message = rawData.toString();
+        console.log({rawData});
+
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN) client.send(`server broadcast: ${message}`);
+        })
+    })
+
+    socket.on('error',(err)=>{
+        console.error(`Error: ${err.message}: ${ip}`);
+    })
+    socket.on('close', () => {
+        console.log('CLIENT DISCONNECTED');
+    })
+}); 
+
+console.log('WebSocket server is running on ws://localhost:8080');
